9b5bffc8c15253ad73b89f0036b4e73d94a8f883
lixkelik <lixkelik@gmail.com>
2023-05-03 14:42:11 +0700

    Added firebase, firestore and firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:seekers/constant/constant_builder.dart';
+import 'package:seekers/firebase_options.dart';
 import 'package:seekers/view/main_page.dart';
 import 'package:flutter/services.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
